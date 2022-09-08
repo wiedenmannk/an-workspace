3703b473f68f0862918f1b0904b51851cc2b4713
wiedenmannk <klaus.wiedenmann@yahoo.de>
2022-09-08 11:50:22 +0200

    indexing libaries

--- projects/skeleton/src/app/modules/forms/forms.module.ts
+++ projects/skeleton/src/app/modules/forms/forms.module.ts
@@ -1,7 +1,7 @@
 import { MainModule } from '@modules/main/main.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StarterLibModule } from 'starter-lib/starter-lib.module';
+import { StarterLibModule } from 'starter-lib';
 
 import { FormsRoutingModule } from './forms-routing.module';
 import { FormsOverviewComponent } from '../../forms/forms-overview/forms-overview.component';
