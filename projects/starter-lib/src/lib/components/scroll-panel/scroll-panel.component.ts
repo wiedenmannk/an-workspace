b8c7e2ef9f91385bae021ac1162e7b3fb8bfc103
wiedenmannk <klaus.wiedenmann@yahoo.de>
2022-11-22 16:13:25 +0100

    eslint fix

--- projects/starter-lib/src/lib/components/scroll-panel/scroll-panel.component.ts
+++ projects/starter-lib/src/lib/components/scroll-panel/scroll-panel.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'scroll-panel',
-  templateUrl: './scroll-panel.component.html',
-  styleUrls: ['./scroll-panel.component.css']
+	selector: "scroll-panel",
+	templateUrl: "./scroll-panel.component.html",
+	styleUrls: ["./scroll-panel.component.css"],
 })
 export class ScrollPanelComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
