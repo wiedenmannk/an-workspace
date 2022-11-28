d86c0942af6c12508de4d4369f609d6438254c37
wiedenmannk <klaus.wiedenmann@yahoo.de>
2022-11-28 16:32:53 +0100

    first form

--- projects/skeleton/src/app/demo/demo.module.ts
+++ projects/skeleton/src/app/demo/demo.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from "@angular/common";
 import { DemoRoutingModule } from "./demo-routing.module";
 import { FormsComponent } from "./forms/forms.component";
 import { TableComponent } from "./table/table.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
 	declarations: [FormsComponent, TableComponent],
-	imports: [CommonModule, MainModule, DemoRoutingModule],
+	imports: [CommonModule, MainModule, DemoRoutingModule, ReactiveFormsModule],
 })
 export class DemoModule {}
--- projects/skeleton/src/app/demo/forms/forms.component.ts
+++ projects/skeleton/src/app/demo/forms/forms.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-forms',
-  templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+	selector: "app-forms",
+	templateUrl: "./forms.component.html",
+	styleUrls: ["./forms.component.scss"],
 })
 export class FormsComponent implements OnInit {
+	jobForm = new FormGroup({
+		firstName: new FormControl(""),
+		lastName: new FormControl(""),
+	});
+	preview: string = "";
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	save(): void {
+		this.preview = JSON.stringify(this.jobForm.value);
+	}
 }
