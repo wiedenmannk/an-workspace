0b7443dbfb06b2eb218579f351c9ec41f6403853
wiedenmannk <klaus.wiedenmann@yahoo.de>
2022-11-29 16:22:52 +0100

    form validators

--- projects/skeleton/src/app/demo/forms/forms.component.ts
+++ projects/skeleton/src/app/demo/forms/forms.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import {
+	FormBuilder,
+	FormControl,
+	FormGroup,
+	Validators,
+} from "@angular/forms";
+import { createPasswordStrengthValidator } from "@demo/validator/password-validator";
 
 interface JobForm {
-	firstName: FormControl<string | null>;
-	lastName: FormControl<string | null>;
+	firstName: FormControl<null | string>;
+	lastName: FormControl<null | string>;
+	password: FormControl<null | string>;
 }
 
 @Component({
@@ -12,18 +19,38 @@ interface JobForm {
 	styleUrls: ["./forms.component.scss"],
 })
 export class FormsComponent implements OnInit {
-	jobForm: FormGroup<JobForm> = this.fb.group({
-		firstName: [""],
+	jobFormx: FormGroup<JobForm> = this.fb.group({
+		firstName: ["", Validators.required],
 		lastName: [""],
+		password: ["", [Validators.required, createPasswordStrengthValidator()]],
 	});
+
+	jobForm: FormGroup<JobForm> = this.fb.group({
+		firstName: this.fb.control<string | null>(null, {
+			validators: [Validators.required],
+		}),
+		lastName: this.fb.control<string | null>(null),
+		password: this.fb.control<string | null>(null, {
+			validators: [Validators.required, createPasswordStrengthValidator()],
+		}),
+	});
+	externalData = { lastName: "Wiedenmann" };
 	preview: string = "";
 	constructor(private fb: FormBuilder) {}
 
 	ngOnInit(): void {
+		this.jobForm.patchValue(this.externalData);
+		this.logForm();
+	}
+
+	logForm(): void {
 		console.log("form firstname", this.jobForm.value.firstName);
+		console.log("controls firstname", this.jobForm.controls.firstName);
+		console.log("form", this.jobForm);
 	}
 
 	save(): void {
 		this.preview = JSON.stringify(this.jobForm.value);
+		this.logForm();
 	}
 }
