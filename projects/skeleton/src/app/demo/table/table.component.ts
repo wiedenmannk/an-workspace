9fb2218b2286cfd8bf3a784bd9cdb90da7b55214
wiedenmannk <klaus.wiedenmann@yahoo.de>
2022-12-22 15:55:15 +0100

    scrolling tables

--- projects/skeleton/src/app/demo/table/table.component.ts
+++ projects/skeleton/src/app/demo/table/table.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+	selector: "app-table",
+	templateUrl: "./table.component.html",
+	styleUrls: ["./table.component.scss"],
 })
 export class TableComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	data = [] as any;
+	constructor() {
+		const line = {
+			name: "Klaus",
+			lastName: "Wiedenmann",
+			street: "Osterstraße 20",
+			city: "Pollenfeld",
+		};
+		this.data = Array.from({ length: 100 }, () => line);
+		console.log("data", this.data);
+	}
+	ngOnInit(): void {}
 }
--- projects/starter-lib/src/public-api.ts
+++ projects/starter-lib/src/public-api.ts
@@ -2,8 +2,8 @@
  * Public API Surface of starter-lib
  */
 
-export * from './lib/starter-lib.service';
-export * from './lib/starter-lib.component';
-export * from './lib/starter-lib.module';
-export * from './lib/primeng/primeng.module';
-export * from './lib/components/scroll-panel/scroll-panel.component';
+export * from "./lib/starter-lib.service";
+export * from "./lib/starter-lib.component";
+export * from "./lib/starter-lib.module";
+export * from "./lib/primeng/primeng.module";
+export * from "./lib/components/scroll-panel/scroll-panel.component";
