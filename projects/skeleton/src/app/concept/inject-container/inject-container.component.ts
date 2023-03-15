2c0cc3fb9e851c0128dfeded4ce287b99c49fc74
wiedenmannk <klaus.wiedenmann@yahoo.de>
2023-03-15 18:17:28 +0100

    injector function

--- projects/skeleton/src/app/concept/inject-container/inject-container.component.ts
+++ projects/skeleton/src/app/concept/inject-container/inject-container.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-inject-container',
-  templateUrl: './inject-container.component.html',
-  styleUrls: ['./inject-container.component.scss']
+	selector: "app-inject-container",
+	templateUrl: "./inject-container.component.html",
+	styleUrls: ["./inject-container.component.scss"],
 })
 export class InjectContainerComponent implements OnInit {
+	dataModel: Array<{ label: string; action: string; css: string }> = [
+		{
+			label: "test1",
+			action: "doIt",
+			css: "p-button-primary",
+		},
+		{
+			label: "test2",
+			action: "make It So",
+			css: "p-button-secondary",
+		},
+		{
+			label: "test3",
+			action: "make some action",
+			css: "p-button-help",
+		},
+	];
+	chosenAction: string = "";
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	recieveAction(event: any): void {
+		this.chosenAction = event;
+	}
 }
--- projects/skeleton/src/app/concept/inject-function/inject-function.component.ts
+++ projects/skeleton/src/app/concept/inject-function/inject-function.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	Input,
+	Output,
+	TemplateRef,
+	EventEmitter,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-inject-function',
-  templateUrl: './inject-function.component.html',
-  styleUrls: ['./inject-function.component.scss']
+	selector: "app-inject-function",
+	templateUrl: "./inject-function.component.html",
+	styleUrls: ["./inject-function.component.scss"],
 })
 export class InjectFunctionComponent implements OnInit {
+	@Input() dataModel!: any[];
+	@Input() template!: TemplateRef<any>;
+	@Output() doAction = new EventEmitter<any>();
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	action(item: any): void {
+		this.doAction.emit(item);
+	}
 }
